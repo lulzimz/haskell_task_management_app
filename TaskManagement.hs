f56884a28efd13197cc7a1bb46ecd9ad0fb6c7a4
lulzimz <jimmy.zenelaj@gmail.com>
2024-04-23 22:40:33 +0200

    defined a task model with it's data types as initital commit

---
+++ TaskManagement.hs
@@ -0,0 +1,9 @@
+-- Define Task data type
+data Task = Task {
+    taskId :: Int,
+    title :: String,
+    description :: String,
+    date :: String,
+    priority :: Int,
+    completed :: Bool
+} deriving (Show)
